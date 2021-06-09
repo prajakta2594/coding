5c36e44b2afd3aa4c52a3348146e10ebc7ff4280
prajakta <83332545+prajakta2594@users.noreply.github.com>
2021-06-09 17:01:06 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello
 {
     public static void main(String[] args) {
-        System.out.println("Welcome !! Happy Coding :)");
+        System.out.println("Welcome !! Happy Coding :");
         System.out.println("Apply some changes to check webhook works");
         System.out.println("Again Apply some changes to check webhook works");
         System.out.println("some changes to check webhook works");
@@ -9,7 +9,8 @@ public class Hello
          System.out.println("changes suggested");
         System.out.println("changing");
         System.out.println("changes done successfully");
-        System.out.println("Coding :)");
-        System.out.println("Bye !! End of Coding :)");
+        System.out.println("Coding :");
+        System.out.println("Webhook not working");
+        System.out.println("Bye !! End of Coding :");
     }
 }
